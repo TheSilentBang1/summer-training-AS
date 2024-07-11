e650844a4b426ad16ab842ac34905ca28222521f
arpit sharma <arpitsharma2003.123@gmail.com>
2024-07-11 14:06:40 +0530

    day2

--- Week0/day0/day0.java
+++ Week0/day0/day0.java
@@ -1,3 +1,4 @@
+package day0;
 class day0 {
     public static void main(String args[]){
         System.out.println("Hello");
---
+++ Week0/day1/datatypes.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class datatypes {
+    public static void main(String args[]){
+        // int: stores integer values, 4 bytes
+        // float: stores decimal values upto 6 decimals, 4 bytes
+        // double: stores decimal values upto 15 decimals,8 bytes
+        // bool: true or false, 1 bit
+        // char: character, 2 byte
+        // long: integer, 8 bytes
+        // short: integer, 2 bytes\ 
+        // operators: 
+        // arithmetic operators:
+        
+    }
+    
+}
