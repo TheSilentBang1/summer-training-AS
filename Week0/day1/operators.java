d2960f4d9462b5edbfc8e879557dba7e7fe2aab3
arpit sharma <arpitsharma2003.123@gmail.com>
2024-07-11 14:06:40 +0530

    day1

---
+++ Week0/day1/operators.java
@@ -0,0 +1,49 @@
+package day1;
+
+public class operators {
+    public static void main(String args[]){
+        // arithmatic operators
+        //+,-,*,/,%,++,--
+        // int a = 5,b = 6;
+        // int c = a+b;
+        // System.out.println(c);
+        // a++;b--; 
+        // a = a+b; 
+        // b = b+a; 
+        // a = a-b; 
+        // b = b-a; 
+        // int c = a+b; 
+        // System.out.println(c);
+
+
+        //comparision operators
+        //<,>,==,<=,>=
+
+        //logical operators
+
+        //assignment operators
+
+        //bitwise operators
+
+        //loops: for,do while,while
+        //using for print sum of all numbers from 1 to 25
+        // int sum = 0;
+        // for(int i = 1;i<=25;i++){
+        //     sum = sum+i;
+        //     System.out.print(sum+" ");
+        // }
+
+        int i =5;
+        while(i<15){
+            if(i%7==0&&i%2==0){
+                System.out.println(i);
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+
+}
