c7eb94c75676650ab6731c190846f0392251ba3e
arpit sharma <arpitsharma2003.123@gmail.com>
2024-07-17 02:04:32 +0530

    review :)

--- Week0/day4/bubble_sort.java
+++ Week0/day4/bubble_sort.java
@@ -1,5 +1,34 @@
 package day4;
 
+import java.util.Scanner;
+
 public class bubble_sort {
-    
+
+    public static void bubbleSort(int [] arr){
+        for(int turn = 1;turn<arr.length;turn++){
+            for(int i = 0;i<arr.length-turn;i++){
+                if(arr[i]>arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int [] arr = new int[10];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0;i<arr.length;i++){
+                arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        bubbleSort(arr);
+
+        for(int i:arr){
+            System.out.print(i+" ");
+    }
+}
+
 }
--- Week0/day4/reversing_partof_arr.java
+++ Week0/day4/reversing_partof_arr.java
@@ -1,4 +1,5 @@
 package day4;
+import java.util.*;
 
 public class reversing_partof_arr {
     public static void swapStartEnd(int [] arr,int start,int end){ 
@@ -7,13 +8,33 @@ public class reversing_partof_arr {
         arr[end] = temp;
     }
 
-    public static void reverse_Arr(int [] arr){ 
-        int start = 0;
-        int end = (arr.length - 1);
+    public static void reversePartOfArr(int [] arr,int i,int j){ 
+        int start = i;
+        int end = j;
         while(start<=end){
             swapStartEnd(arr, start, end);
             start++;
             end--;
+        }
+    }
+    public static void main(String args[]){
+        
+        int [] arr = new int [10];
+        System.out.println("Enter array elements: ");
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter reverse start index: ");
+        int i = sc.nextInt();
+        System.out.println("Enter reverse end index: ");
+        int j = sc.nextInt();
+        sc.close();
+        
+        reversePartOfArr(arr,i,j);
+        for(int k:arr){
+            System.out.print(k+" ");
         }
     }
     
